641e6517c3fe5ba614a88c4fcab1989487936b53
Abdullah Hafizh <abdullah.hafizh@mdd.co.id>
2020-10-10 13:22:33 +0700

    Set data created attribute and append year

--- app/Data.php
+++ app/Data.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Data extends Model
 {
-    //
+    protected $hidden = ['id','updated_at'];
+    protected $appends = ['tahun'];
+
+    public function getCreatedAtAttribute($value)
+    {
+    	return date('Y-m-d H:i:s', strtotime($value));
+    }
+
+    public function getTahunAttribute()
+    {
+    	return date('Y', strtotime($this->created_at));
+    }
 }
