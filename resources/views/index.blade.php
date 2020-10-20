d735ab2aaebd146ba37c96b8f0d039232893d636
Developer Prodigi <devprodigi2020@gmail.com>
2020-10-20 16:35:38 +0700

    change introduce on page home

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,23 +1,26 @@
 @extends('layouts.frontend.app', ['footerPage' => ''])
 
 @section('title')
-    Beranda
+Beranda
 @endsection
 @section('content')
-<div class="jumbotron jumbotron-fluid page-content" style="background-image: url('{{ asset('assets/images/home/jumbotron-b.jpg')  }}')">
+<div class="jumbotron jumbotron-fluid page-content"
+    style="background-image: url('{{ asset('assets/images/home/jumbotron-b.jpg')  }}')">
     <div class="container text-center" data-aos="zoom-in" data-aos-delay="800">
         <img src="{{asset('/assets/logo-utama.png')}}" alt="" style="width: 400px; margin-top: 40px;">
         <h3 class="text-center mt-4"> <strong>SATU DATA KETENAGAKERJAAN</strong> </h3>
         <form method="GET" action="dataset.html">
             <div class="form-group mb-3">
-                <input type="text" class="form-control" id="e_keyword" placeholder="Masukan kata kunci pencarian ..." name="q">
+                <input type="text" class="form-control" id="e_keyword" placeholder="Masukan kata kunci pencarian ..."
+                    name="q">
             </div>
             <button type="submit" class="btn btn-success">
-        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewbox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3" >
-            <circle cx="10.5" cy="10.5" r="7.5"></circle>
-            <line x1="21" y1="21" x2="15.8" y2="15.8"></line>
-        </svg><b>Cari Data</b>&nbsp; &nbsp; &nbsp;
-    </button>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewbox="0 0 24 24" fill="none"
+                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="mx-3">
+                    <circle cx="10.5" cy="10.5" r="7.5"></circle>
+                    <line x1="21" y1="21" x2="15.8" y2="15.8"></line>
+                </svg><b>Cari Data</b>&nbsp; &nbsp; &nbsp;
+            </button>
         </form>
     </div>
 </div>
@@ -26,23 +29,28 @@
 <section class="wow-pad bg-white" style="margin-top: 100px;">
     <div class="container">
         <div class="container">
-            <br/>
-            <h1 class="text-center">
+            <br />
+            {{-- <h1 class="text-center">
                 <strong>Ketenagakerjaan Dalam Data</strong>
-            </h1><br/><br>
+            </h1><br /><br> --}}
             <div class="row">
-                <div class="col-12 col-md-6 col-lg-6 col-xl-6 col-sm-12 col-xs-12">
+                {{-- <div class="col-12 col-md-6 col-lg-6 col-xl-6 col-sm-12 col-xs-12">
                     <div id="neet" style="height: 300px; width: 100%;"></div>
                 </div>
                 <div class="col-12 col-md-6 col-lg-6 col-xl-6 col-sm-12 col-xs-12">
                     <div id="covid2" style="height: 300px; width: 100%;"></div>
+                </div> --}}
+                <div class="col-12" data-aos="zoom-in" data-aos-delay="200">
+                    <img src="{{asset('assets/images/home/infografik.png')}}" alt="" width="100%">
                 </div>
             </div>
         </div>
+    </div>
 </section>
 
-<div class="benefit mb-4" style="margin-top: 180px;" style="background-image: url('{{ asset('assets/images/home/benefit-bg1.jpg')  }}')">
-    <h1><strong>Layanan Kami</strong></h1>
+<div class="benefit mb-4" style="margin-top: 180px;"
+    style="background-image: url('{{ asset('assets/images/home/benefit-bg1.jpg')  }}')">
+    <h1><strong>Penyelenggara</strong></h1>
     <div class="container">
         <div class="row">
             <div class="benefit-bg-1 col-lg-5 " data-aos="fade-right" data-aos-delay="100"></div>
@@ -51,32 +59,23 @@
         <div class="benefit-content row">
             <div class="col-lg-2"></div>
             <div class="benefit-content-description benefit-bg-2 col-lg-8" data-aos="zoom-in" data-aos-delay="100">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <img src="{{asset('/assets/images/home/benefit-icon1.svg')}}" alt="" class="mt-4 mb-4">
-                        <h3 class="mt-2 mb-2">Pengumpulan Data Daring</h3>
-                        <p>Pengumpulan data daring dilakukan di dalam satudata dan diproses langsung melalui metode-metode statistik dari berbagai pakar statistika</p>
-                    </div>
-                    <div class="col-lg-6">
-                        <img src="{{ asset('/assets/images/home/benefit-icon3.svg')}}" alt="" class="mt-4 mb-4">
-                        <h3 class="mt-2 mb-2">Analisis Data</h3>
-                        <p>Analisis data melalui Data Science ditampilkan beserta data yang dicari</p>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-6"> <img src="{{ asset('/assets/images/home/benefit-icon2.svg')}}" alt="" class="mt-4 mb-4">
-                        <h3 class="mt-2 mb-2">Klasifikasi Data</h3>
-                        <p>Data telah terklasifkasi sehingga memudahkan pengguna untuk menggunakan data tersebut demi keperluan akademik</p>
-                    </div>
-                    <div class="col-lg-6">
-                        <img src="{{ asset('/assets/images/home/benefit-icon4.svg')}}" alt="" class="mt-4 mb-4">
-                        <h3 class="mt-2 mb-2">Penyajian</h3>
-                        <p>data disajikan seemenarik mungkin melalui metode statistik sehingga memudahkan pengguna untuk membaca data tersebut</p>
-                    </div>
-                </div>
-                <img src="{{ asset('assets/images/home/benefit-icon2.svg')}}" alt="" class="mt-4 mb-4">
-                <h3 class="mt-2 mb-2">Permintaan Data untuk Akademik</h3>
-                <p>Satudata memberikan layanan untuk sivitas akademika untuk mengajukan permintaan data untuk keperluan pendidikan</p>
+                <img src="{{asset('assets/images/home/benefit-icon1.svg')}}" alt="" class="mt-4 mb-4">
+                <h1 class="mt-2 mb-2">Pembina Data</h1>
+                <p>Menteri yang menyelenggarakan urusan pemerintahan di bidang ketenagakerjaan</p>
+                <img src="{{asset('assets/images/home/benefit-icon2.svg')}}" alt="" class="mt-4 mb-4">
+                <h1 class="mt-2 mb-2">Pengarah</h1>
+                <p>Pejabat tinggi madya di kementerian</p>
+                <img src="{{asset('assets/images/home/benefit-icon3.svg')}}" alt="" class="mt-4 mb-4">
+                <h1 class="mt-2 mb-2">Walidata</h1>
+                <p>Unit kerja di kementerian yang mempunyasi tugas pengelolaan data ketenagakerjaan (Pusdatinaker)</p>
+                <img src="{{asset('assets/images/home/benefit-icon4.svg')}}" alt="" class="mt-4 mb-4">
+                <h1 class="mt-2 mb-2">Produsen Data</h1>
+                <p>Unit kerja di instansi pusat dan daerah yang menghasilkan data ketenagakerjaan:</p>
+                <Ul type="a">
+                    <li>Unit eselon 2 kemnaker</li>
+                    <li>Unit eselon 2 KL lain/ Instansi Daerah/ Lembaga atau Organisasi nasional ataupun internasional
+                        yang menghasilkan data ketenagakerjaan</li>
+                </Ul>
             </div>
             <div class="col-lg-2"></div>
         </div>
