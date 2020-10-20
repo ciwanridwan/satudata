4fca5d005a5db368e050631403fd916b7ed86f82
Abdullah Hafizh <abdullah.hafizh@mdd.co.id>
2020-10-21 06:30:46 +0700

    Fix if empty data file size

--- app/Data.php
+++ app/Data.php
@@ -77,7 +77,27 @@ class Data extends Model
 
 		$file = public_path('files/' . $this->file);
 		if (file_exists($file)) {
-			return filesize($file);
+			$size = filesize($file);
+
+			if ($size >= 1073741824) {
+				$size = number_format($size / 1073741824, 2) . 'GB';
+			} elseif ($size >= 1048576) {
+				$size = number_format($size / 1048576, 2) . ' MB';
+			} elseif ($size >= 1024) {
+				$size = number_format($size / 1024, 2) . ' KB';
+			} elseif ($size > 1) {
+				$size = $size . ' bytes';
+			} elseif ($size == 1) {
+				$size = $size . ' byte';
+			} else {
+				$size = '0 bytes';
+			}
+
+			$update = self::where('id', $this->id)->first();
+			$update->size_files = $size;
+			$update->update();
+
+			return $size;
 		}
 	}
 }
