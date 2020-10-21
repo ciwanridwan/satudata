71770c37532c5cd637603d38fc47443c876e118b
Abdullah Hafizh <abdullah.hafizh@mdd.co.id>
2020-10-21 20:47:42 +0700

    Fix wrong publikasi file size

--- app/Publikasi.php
+++ app/Publikasi.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Schema;
 
 class Publikasi extends Model
 {
-	protected $appends = ['date'];
+	protected $appends = ['date','size_file'];
 
 	public static function search($query, $year = null, $request)
 	{
@@ -48,5 +48,31 @@ class Publikasi extends Model
 	public function getDateAttribute()
 	{
 		return date('Y-m-d', strtotime($this->created_at));
+	}
+
+	public function getSizeFileAttribute()
+	{
+		$file_size = filesize(public_path('files/'. $data->file));
+
+		if ($file_size >= 1073741824) {
+			$file_size = number_format($file_size / 1073741824, 2).'GB';
+		}
+		elseif ($file_size >= 1048576) {
+			$file_size = number_format($file_size / 1048576, 2) . ' MB';
+		}
+		elseif ($file_size >= 1024) {
+			$file_size = number_format($file_size / 1024, 2) . ' KB';
+		}
+		elseif ($file_size > 1) {
+			$file_size = $file_size . ' bytes';
+		}
+		elseif ($file_size == 1) {
+			$file_size = $file_size . ' byte';
+		}
+		else {
+			$file_size = '0 bytes';
+		}
+
+		return $file_size;
 	}
 }
