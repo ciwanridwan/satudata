2b5caa3ad8c72cf3d6f93bd112266bc3e3ea2dd3
Abdullah Hafizh <abdullah.hafizh@mdd.co.id>
2020-10-23 06:42:51 +0700

    Try add thumbnail for infographic #2

--- resources/views/pages/details/infograpik.blade.php
+++ resources/views/pages/details/infograpik.blade.php
@@ -48,7 +48,7 @@
         <div class="col-lg-4 col-md-6 col-sm-12 mt-5 mb-5">
             <a href="{{ route('pages.detail.infograpik', $value->plain_id) }}">
                 <div class="card card-produk" data-aos="fade-up" data-aos-delay="100">
-                    <img class="card-img-top" src="{{ asset('/files/infografik/' . $value->gambar) }}" alt="Card image cap" />
+                    <img class="card-img-top" src="{{ asset($value->thumbnail) }}" style="object-fit: cover;" alt="Card image cap" />
                     <div class="card-body">
                         <div class="row judul-berita">
                             <div class="col-7">
