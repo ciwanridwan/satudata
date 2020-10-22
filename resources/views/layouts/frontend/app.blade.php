0346800b7f4041853f3ef31c95da3b5efdc2bff3
Abdullah Hafizh <abdullah.hafizh@mdd.co.id>
2020-10-22 10:02:58 +0700

    Add fontawesome kit

--- resources/views/layouts/frontend/app.blade.php
+++ resources/views/layouts/frontend/app.blade.php
@@ -12,6 +12,7 @@
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
     <link href="{{ asset('/assets/style/main.css')}}" rel="stylesheet" />
 
+    <script src="https://kit.fontawesome.com/231b668aca.js" crossorigin="anonymous"></script>
     <script src="https://code.highcharts.com/highcharts.js"></script>
     <script src="https://code.highcharts.com/modules/exporting.js"></script>
     <script src="https://code.highcharts.com/modules/accessibility.js"></script>
